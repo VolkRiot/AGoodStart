63bb98bbc3c09ed012a8f9a333a6d9f1e2cdaf10
Mikhail Metrikin <metrikin@gmail.com>
2018-04-07 21:14:51 -0700

    Initial Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Mikhail Metrikin on 4/7/18.
+//  Copyright © 2018 Mikhail Metrikin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
