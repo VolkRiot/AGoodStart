01b18a6b3a92e66c5d3efeb934592ffa38387155
Mikhail Metrikin <metrikin@gmail.com>
2018-04-15 16:24:19 -0700

    Re learning C

--- AGoodStart/main.c
+++ AGoodStart/main.c
@@ -9,7 +9,13 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    float num1 = 3.14;
+    float num2 = 1.445;
+    
+    double sum = num1 + num2;
+    
+    printf("The sum of the two numbers is stored in a double as %f.\n", sum);
+    
+    
     return 0;
 }
