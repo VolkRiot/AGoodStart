094f17619b2605e4099cd98e6d8b78dfcce36704
Mikhail Metrikin <metrikin@gmail.com>
2018-07-04 20:39:27 -0700

    Integers in C

---
+++ Numbers/Numbers/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Mikhail Metrikin on 7/4/18.
+//  Copyright © 2018 Mikhail Metrikin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int x = 255;
+    printf("x is %d. \n", x);
+    
+    return 0;
+}
