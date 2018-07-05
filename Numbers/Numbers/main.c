08893ef74cb67fe01dd325e062c407b1e58e0570
Mikhail Metrikin <metrikin@gmail.com>
2018-07-04 20:55:00 -0700

    SAVEPOINT

--- Numbers/Numbers/main.c
+++ Numbers/Numbers/main.c
@@ -8,9 +8,24 @@
 
 #include <stdio.h>
 
+void multiply()
+{
+    printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
+}
+
 int main(int argc, const char * argv[]) {
-    int x = 255;
-    printf("x is %d. \n", x);
+    unsigned long x = 255;
+    
+    printf("x is %lu. \n", x);
+    
+    // No need to adapt these for unsigned.
+    printf("In octal, x is %lo.\n", x);
+    printf("In hexadecimal x is %lx. \n", x);
+    
+    // Casting and int division
+    printf("11 / 3.0 = %f\n", 11 / (float)3);
+    
+    multiply();
     
     return 0;
 }
