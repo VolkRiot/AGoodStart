2589f52f35e0ce9fadc017851cbb92a85ad760ee
Mikhail Metrikin <metrikin@gmail.com>
2018-10-08 21:13:13 -0700

    Addresses/pointers

--- Address-C/Address-C/main.c
+++ Address-C/Address-C/main.c
@@ -10,7 +10,11 @@
 
 int main(int argc, const char * argv[]) {
     int i = 17;
+    int *addressOfI = &i;
+    
+    printf("i sores its value at %p\n", addressOfI);
     printf("i store its values at %p\n", &i);
     printf("This function starts at %p\n", &main);
+    printf("the int stored at address addressOfI is %d\n", *addressOfI);
     return 0;
 }
